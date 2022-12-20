2cec4c27686e92816ac598b15bed835e1cb26692
Masih Daneshfar <44798644+masih-daneshfar@users.noreply.github.com>
2022-12-20 21:07:25 +0330

    add User api route
    Signed-off-by: Masih Daneshfar <44798644+masih-daneshfar@users.noreply.github.com>

--- server/src/route/user.route.ts
+++ server/src/route/user.route.ts
@@ -1,39 +1,61 @@
-import { Request, Response ,Express} from "express";
+import { Request, Response, Router } from "express";
 import { AppDataSource } from "../config/data-source";
 import { User } from "../entity/User";
 
-const UserRoute = (app: Express) => {
-  // register routes
-  app.get("/users", async function (req: Request, res: Response) {
+const router = Router();
+// register routes
+router.get("/", async function (req: Request, res: Response, next) {
+  try {
     const users = await AppDataSource.getRepository(User).find();
     res.json(users);
-  });
+  } catch (error) {
+    next({ ...error, statusCode: 500 });
+  }
+});
 
-  app.get("/users/:id", async function (req: Request, res: Response) {
-    const results = await AppDataSource.getRepository(User).findOneBy({
+router.get("/:id", async function (req: Request, res: Response, next) {
+  try {
+    const results = await AppDataSource.getRepository(User).findOneByOrFail({
       id: Number(req.params.id),
     });
-    return res.send(results);
-  });
+    return res.json(results);
+  } catch (error) {
+    next({ ...error, statusCode: 404 });
+  }
+});
 
-  app.post("/users", async function (req: Request, res: Response) {
-    const user = await AppDataSource.getRepository(User).create(req.body);
+router.post("/new", async function (req: Request, res: Response, next) {
+  try {
+    const user = AppDataSource.getRepository(User).create(req.body);
     const results = await AppDataSource.getRepository(User).save(user);
     return res.send(results);
-  });
+  } catch (error) {
+    next({ ...error, statusCode: 400 });
+  }
+});
 
-  app.put("/users/:id", async function (req: Request, res: Response) {
+router.put("/:id", async function (req: Request, res: Response, next) {
+  try {
     const user = await AppDataSource.getRepository(User).findOneBy({
       id: Number(req.params.id),
     });
     AppDataSource.getRepository(User).merge(user, req.body);
     const results = await AppDataSource.getRepository(User).save(user);
     return res.send(results);
-  });
+  } catch (error) {
+    next({ ...error, statusCode: 404 });
+  }
+});
 
-  app.delete("/users/:id", async function (req: Request, res: Response) {
-    const results = await AppDataSource.getRepository(User).delete(req.params.id);
+router.delete("/:id", async function (req: Request, res: Response, next) {
+  try {
+    const results = await AppDataSource.getRepository(User).delete(
+      req.params.id
+    );
     return res.send(results);
-  });
-};
-export default UserRoute;
+  } catch (error) {
+    next({ ...error, statusCode: 404 });
+  }
+});
+
+export default router;
