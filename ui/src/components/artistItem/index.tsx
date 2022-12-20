5176c6c9a5273b7fd498198e281f9c57b2dd02cd
Masih Daneshfar <44798644+masih-daneshfar@users.noreply.github.com>
2022-12-20 21:15:02 +0330

    add artistItem component
    Signed-off-by: Masih Daneshfar <44798644+masih-daneshfar@users.noreply.github.com>

---
+++ ui/src/components/artistItem/index.tsx
@@ -0,0 +1,27 @@
+import Anchor from "@components/anchor";
+import styles from "./artistItem.module.css";
+
+interface ArtistItemType {
+  title?: string;
+  summery?: string;
+  href?: string;
+  avatarUrl?: string;
+}
+function ArtistItem({ href = "", summery, title,avatarUrl }: ArtistItemType) {
+  return (
+    <>
+      <div className={styles.artistContainer}>
+        <div className={styles.artistAvatar}>
+          <img src={avatarUrl} alt='' />
+        </div>
+        <div className={styles.artistInfoContainer}>
+          <h3 className={styles.artistTitle}>{title}</h3>
+          <p className={styles.artistSummery}>{summery}</p>
+          <Anchor to={href}>بیشتر ...</Anchor>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ArtistItem;
