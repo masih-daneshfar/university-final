02f7e51d5caa0fb7d89b49fda1e2463cdb7f72d9
Masih Daneshfar <44798644+masih-daneshfar@users.noreply.github.com>
2022-12-20 21:06:30 +0330

    add FAQ api route
    Signed-off-by: Masih Daneshfar <44798644+masih-daneshfar@users.noreply.github.com>

---
+++ server/src/route/faq.route.ts
@@ -0,0 +1,55 @@
+import { Request, Response, Router } from "express";
+import { AppDataSource } from "../config/data-source";
+import { Faq } from "../entity/Faq";
+
+const router = Router();
+// register routes
+router.get("/", async function (req: Request, res: Response, next) {
+  try {
+    const faqs = await AppDataSource.getRepository(Faq).find();
+    res.json(faqs);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async function (req: Request, res: Response, next) {
+  try {
+    const results = await AppDataSource.getRepository(Faq).findOneBy({
+      id: Number(req.params.id),
+    });
+    return res.send(results);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/new", async function (req: Request, res: Response, next) {
+  try {
+    const newFaq = AppDataSource.getRepository(Faq).create(req.body);
+    const results = await AppDataSource.getRepository(Faq).save(newFaq);
+    return res.send(results);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/update/:id", async function (req: Request, res: Response, next) {
+  try {
+    const faq = await AppDataSource.getRepository(Faq).findOneBy({
+      id: Number(req.params.id),
+    });
+    AppDataSource.getRepository(Faq).merge(faq, req.body);
+    const results = await AppDataSource.getRepository(Faq).save(faq);
+    return res.send(results);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/remove/:id", async function (req: Request, res: Response) {
+  const results = await AppDataSource.getRepository(Faq).delete(req.params.id);
+  return res.send(results);
+});
+
+export default router;
